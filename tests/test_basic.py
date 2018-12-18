8f43fbbd59f3548202c70c10eaea7b79ee286fdf
Gabriel <gabriel59kg@gmail.com>
2018-12-17 19:38:55 -0500

    again

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,7 +9,7 @@ basic tests for `polyglot` module.
 """
 
 import pytest
-from polyglot import polyglot
+import polyglot
 
 
 @pytest.mark.parametrize(
@@ -25,6 +25,8 @@ from polyglot import polyglot
     ],
 )
 def test_language_detection(text_input, expected_code, expected_name):
+    from polyglot.text import Text
+
     poly_text = polyglot.Text(text_input)
     assert poly_text.language.code == expected_code
     assert poly_text.language.name == expected_lang
