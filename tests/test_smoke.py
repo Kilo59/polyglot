fece9cbca243b2c9446e3886c8383d687ca75e73
Gabriel <gabriel59kg@gmail.com>
2018-12-17 19:21:13 -0500

    add quickstart test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -15,6 +15,12 @@ def test_for_fire():
     """Make sure nothing blows up while importing polyglot"""
     import polyglot
 
+def test_quick_start_language_detection()
+    """Test the quickstart language detection example"""
+    from polyglot.text import Text, Word
+    text = Text("Bonjour, Mesdames.")
+    assert text.language.code == "fr"
+    assert text.language.name == "French"
 
 if __name__ == "__main__":
     pytest.main(["-v"])
